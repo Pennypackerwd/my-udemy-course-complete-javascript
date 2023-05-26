6a686fae54c7b696583f036aa91d5ee556313e48
Cliff <cliffordernst@gmail.com>
2023-05-26 15:41:24 -0400

    added new branch

---
+++ section_1/script.js
@@ -0,0 +1,19 @@
+document.write('it finally worked');
+alert('work please');
+/*
+document.write('pennypacker');
+document.write('pennypacker');
+document.write('pennypacker');
+document.write('pennypacker');
+document.write('pennypacker');
+*/
+
+/*
+***this allows you to make comments in your code, do you need the second part
+*/
+
+document.write('pennypacker'); //change to penny later
+
+//console message
+console.log('is this working')
+
